2e85e927fdd8dbf7b7005d321b3a941401b2c4b9
babaasplit <162522120+babaasplit@users.noreply.github.com>
2024-11-13 14:22:53 +0700

    first

---
+++ UAS/app.js
@@ -0,0 +1,74 @@
+var app = angular.module('thriveSoulApp', []);
+
+app.controller('MainController', function($scope) {
+  // Initial page setup
+  $scope.currentPage = 'home';
+  $scope.navigate = function(page) {
+    $scope.currentPage = page;
+  };
+
+  // CRUD logic for Beauty Tips
+  $scope.beautyTips = [];
+  $scope.isEditingBeauty = false;
+  $scope.addOrUpdateBeauty = function() {
+    if ($scope.isEditingBeauty) {
+      // Update existing beauty tip
+      var index = $scope.beautyTips.indexOf($scope.editingBeauty);
+      $scope.beautyTips[index] = angular.copy($scope.beautyItem);
+      $scope.isEditingBeauty = false;
+    } else {
+      // Add new beauty tip
+      $scope.beautyTips.push(angular.copy($scope.beautyItem));
+    }
+    $scope.clearBeautyForm();
+  };
+  $scope.deleteBeauty = function(item) {
+    var index = $scope.beautyTips.indexOf(item);
+    $scope.beautyTips.splice(index, 1);
+  };
+  $scope.editBeauty = function(item) {
+    $scope.isEditingBeauty = true;
+    $scope.editingBeauty = item;
+    $scope.beautyItem = angular.copy(item);
+  };
+  $scope.clearBeautyForm = function() {
+    $scope.beautyItem = {};
+    $scope.isEditingBeauty = false;
+  };
+
+  // CRUD logic for Food Recipes
+  $scope.foodItems = [];
+  $scope.isEditingFood = false;
+  $scope.addOrUpdateFood = function() {
+    if ($scope.isEditingFood) {
+      // Update existing food recipe
+      var index = $scope.foodItems.indexOf($scope.editingFood);
+      $scope.foodItems[index] = angular.copy($scope.foodItem);
+      $scope.isEditingFood = false;
+    } else {
+      // Add new food recipe
+      $scope.foodItems.push(angular.copy($scope.foodItem));
+    }
+    $scope.clearFoodForm();
+  };
+  $scope.deleteFood = function(item) {
+    var index = $scope.foodItems.indexOf(item);
+    $scope.foodItems.splice(index, 1);
+  };
+  $scope.editFood = function(item) {
+    $scope.isEditingFood = true;
+    $scope.editingFood = item;
+    $scope.foodItem = angular.copy(item);
+  };
+  $scope.clearFoodForm = function() {
+    $scope.foodItem = {};
+    $scope.isEditingFood = false;
+  };
+
+  // Subscribe functionality
+  $scope.subscriber = {};
+  $scope.subscribe = function() {
+    alert("Subscribed with email: " + $scope.subscriber.email);
+    $scope.subscriber = {}; // Clear form after submission
+  };
+});
